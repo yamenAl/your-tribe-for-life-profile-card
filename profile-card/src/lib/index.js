c6003088cc5a85caded944585803b030158abb7b
yamenAl <yamen_alsharabi@hotmail.com>
2025-09-17 02:11:37 +0200

    calling logo files in index.js #2

---
+++ profile-card/src/lib/index.js
@@ -0,0 +1,13 @@
+// place files you want to import through the `$lib` alias in this folder.
+// place files you want to import through the `$lib` alias in this folder.
+
+// CSS files
+// export { default as style } from './assets/style.css'
+
+// SVG images (as components)
+export { default as GithubLogo } from './assets/githublogo.svelte'
+export { default as LoctionLogo } from './assets/loctionlogo.svelte'
+export { default as FdndLogo } from './assets/fdndlogo.svelte'
+export { default as PhoneLogo } from './assets/phonelogo.svelte'
+
+
